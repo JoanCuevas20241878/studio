f7e5ab54afa70df735dd20e7940ce685f9db847e
Joan Cuevas <joancuevasp@gmail.com>
2025-12-11 07:39:13 +0000

    Try fixing this error: `Runtime ReferenceError: useMemo is not defined.

--- src/app/(app)/analysis/page.tsx
+++ src/app/(app)/analysis/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState } from 'react';
+import { useState, useMemo } from 'react';
 import { collection, query, where, Timestamp, orderBy } from 'firebase/firestore';
 import { useUser, useFirestore, useCollection, useMemoFirebase } from '@/firebase';
 import type { Expense } from '@/types';
