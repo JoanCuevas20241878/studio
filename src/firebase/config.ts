bcdfd804205e53783831687f221c34984c9d7c13
JoanCuevas20241878 <20241878@itla.edu.do>
2025-12-12 00:08:46 -0400

    Remove API key from firebase config
    Removed sensitive API key from firebase configuration.

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-5012517812-8e7e3",
   "appId": "1:155848506510:web:e9ca28b9e98a796be21ddf",
-  "apiKey": "AIzaSyC7LaLelLT10YBTzr72XC0Fp-W-I3B03m0",
+  "apiKey": "",
   "authDomain": "studio-5012517812-8e7e3.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "155848506510"
